0d87e2e0a3ea477d8ebf22db0a7c45f4fadc2d9b
Yurii Zherebukh <yura7887@gmail.com>
2018-04-29 13:42:30 +0300

    Deleted files from vritualenv

--- virtualenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\lenovo\documents\projects\messageexchangeacs\virtualenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
